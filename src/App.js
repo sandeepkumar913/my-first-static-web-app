d48a1fa8b9f918d4c7c0f0af0875517cf77a6d71
Sandeep Lakkakula <SLakkakula@pods.com>
2023-04-30 00:58:32 -0400

    Commit made from VS Code Azure Static Web Apps

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello {value}</div>;
+  return ( <div class='container'>
+            <h2 class="title">We're down for maintenance.</h2>
+
+            <p>We apologize for the inconvenience and appreciate your patience.</p>
+        </div>);
 }
 
 export default App;
